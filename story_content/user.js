956aec78659a7d87e057dfc28aa91bd414d6809c
leonspencer415 <104797816+leonspencer415@users.noreply.github.com>
2022-05-02 18:10:11 -0400

    LoopNet Marketing Center

---
+++ story_content/user.js
@@ -0,0 +1,16 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6OLw22dTsLN":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = new Audio('music.mp3');
+audio.play();
+}
+
